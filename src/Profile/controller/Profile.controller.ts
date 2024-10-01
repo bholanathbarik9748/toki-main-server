2e8ce6821d36c35e407002fdbebd67aea2f9004d
Bholanath <bholanathbarik9748@gmail.com>
2024-10-01 22:54:44 +0530

    Feat: remove multiple Guard improt into one Guard in profile Controller

--- src/Profile/controller/Profile.controller.ts
+++ src/Profile/controller/Profile.controller.ts
@@ -5,32 +5,28 @@ import { ProfileService } from '../services/Profile.service';
 
 
 @Controller('profile')
+@UseGuards(JwtAuthGuard)
 export class ProfileController {
     constructor(private readonly ProfileService: ProfileService) { }
 
-    // Protect this route using JWT Auth Guard (you can remove this if not using JWT)
-    @UseGuards(JwtAuthGuard)
     @Get('/:id')
     getProfile(@Req() req: Request, @Res() res: Response) {
         // In a real application, this could be fetched from the database
         return this.ProfileService.getProfile(req, res);
     }
 
-    @UseGuards(JwtAuthGuard)
     @Post('/:id')
     createProfile(@Req() req: Request, @Res() res: Response) {
         return this.ProfileService.createProfile(req, res);
     }
 
-    @UseGuards(JwtAuthGuard)
     @Patch('/:id')
     updateProfile(@Req() req: Request, @Res() res: Response) {
         return this.ProfileService.updateProfile(req, res);
     }
 
-    @UseGuards(JwtAuthGuard)
     @Delete('/:id')
-    deleteProfile(@Req() req: Request, @Res() res: Response){
+    deleteProfile(@Req() req: Request, @Res() res: Response) {
         return this.ProfileService.deleteProfile(req, res);
     }
 }
