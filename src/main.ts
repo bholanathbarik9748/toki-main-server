81d026ee8b91d48b8c15298068603361993166ed
Bholanath <bholanathbarik9748@gmail.com>
2024-10-06 23:12:30 +0530

    Feat: implement cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ async function mainApp() {
   app.use(morgan('dev'));
   app.setGlobalPrefix('api/v1');
 
+  // Enable CORS globally
+  app.enableCors({
+    origin: '*', // Adjust this to allow specific domains
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+
   // Apply the filter globally
   app.useGlobalFilters(new UserExceptionFilter());
   await app.listen(3000);
